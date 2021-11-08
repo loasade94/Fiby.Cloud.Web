1583e6ac13ecfb57f981ad860cc896e7f5794fe1
hmoron <hmoron@sapia.com.pe>
2021-11-07 23:11:59 -0500

    qwwwww

---
+++ Fiby.Cloud.Web.Client/Areas/Horario/Controllers/CalendarioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiby.Cloud.Web.Client.Areas.Horario.Controllers
+{
+    [Area("Horario")]
+    public class CalendarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
